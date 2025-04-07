07cde959b6b22dba17ca806cdd45c478610665ac
Liang Chang <A004291@SERCOMM.COM>
2025-04-07 17:19:46 +0800

    feat: initial project

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+
+  {
+    path: "/test",
+    element: <div>Test</div>,
+  },
+
+  {
+    path: "/terrible",
+    element: <div>Terrible</div>,
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
